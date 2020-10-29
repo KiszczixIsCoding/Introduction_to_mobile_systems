1988374161b990ac25985a2aab3dc2c7c0c8561b
KiszczixIsCoding <kamilkiszkozgierski@gmail.com>
2020-10-29 03:06:41 +0100

    Prepare projet files
    Create MainActivity, DrawActivity and DrawView. Add button and listener

---
+++ Paint/app/src/main/java/pl/emb/paint/DrawView.java
@@ -0,0 +1,10 @@
+package pl.emb.paint;
+
+import android.content.Context;
+import android.view.View;
+
+public class DrawView extends View {
+    public DrawView(Context context) {
+        super(context);
+    }
+}
